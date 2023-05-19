cb43153155ec8e72085ff33511cb5873f0e1bcfc
Greg Spies <gstotx@hotmail.com>
2023-05-18 19:51:21 -0500

    change input

--- src/address_cleaner.rb
+++ src/address_cleaner.rb
@@ -5,14 +5,12 @@ require 'smartystreets_ruby_sdk/static_credentials'
 require 'smartystreets_ruby_sdk/us_street'   
 
 class AddressCleaner
-    API_KEY = 'fYjX9vOYZAyN2sn4iWLK'
-    #API_KEY = 'YOUR_API_KEY'
-    AUTH_ID = '3ebd56b7-1c56-c4cb-5096-58c318d98a9e'
-    #AUTH_ID = 'YOUR_AUTH_ID'
+    API_KEY = 'YOUR_API_KEY'
+    AUTH_ID = 'YOUR_AUTH_ID'
 
     def self.run
         client = create_client
-        csv_reader = CSVReader.new(ARGV[0])
+        csv_reader = CSVReader.new('../input.csv')
         addresses = csv_reader.read_csv
         
         validator = AddressValidator.new(client, addresses)
